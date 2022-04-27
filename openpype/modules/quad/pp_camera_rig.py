e2466c8aeffa1db0fdb16861d3f1eb0c4a641fb0
Seyedmohammadreza Hashemizadeh <shashemizadeh@ws101.prs.vfx.int>
2022-04-27 19:11:21 +0200

    clean up. replaced the class with a function

--- openpype/modules/quad/pp_camera_rig.py
+++ openpype/modules/quad/pp_camera_rig.py
@@ -1,233 +1,176 @@
 # -*- coding: utf-8 -*-
 
-import logging
-
 import maya.cmds as cmds
 
 
-# logger
-format = '%(asctime)s %(levelname)s\t%(module)s.%(funcName)s | %(message)s'
-logging.basicConfig(format=format)
-logger = logging.getLogger('pp_camera_rig')
-logger.setLevel(logging.DEBUG)
-
-
-class Camera(object):
-    """ a usefull class for manipulate camera rig.
-    """
-    def __init__(self, camera):
-        super(Camera, self).__init__()
-
-        self.camera = camera
-        self.camera_shape = None
-
-        self.namespace = None
-
-        self.ctrl = {
-            # ctrl
-            "triangle": "triangle_ctrl",
-            "circle": "circle_ctrl",
-            "square": "square_ctrl",
-            "cross": "cross_ctrl",
-            "translateZ": "translateZ_ctrl",
-            "translateY": "translateY_ctrl",
-            # target
-            "target": "target_ctrl",
-            "targetUp": "targetUp_ctrl",
-            # focus
-            "focus": "focus_ctrl"
-        }
-
-        self.init_rig()
-
-    def init_rig(self):
-        """
-        """
-        # init camera shape
-        if cmds.nodeType(self.camera) == "transform":
-            self.camera_shape = self._get_camera_shape(transform=self.camera)
-        elif cmds.nodeType(self.camera) == "camera":
-            # user as provided a camera shape
-            # we are smart and flexible
-            self.camera_shape = self.camera
-            self.camera = cmds.listRealtives(self.camera_shape, parent=True)[0]
-
-        # init namespace
-        self.namespace = self._get_namespace(node=self.camera)
-
-    def _get_namespace(self, node):
-        """
-        """
-        self.namespace = None
-        if ":" in node:
-            node_short = node
-            # split by :
-            if "|" in node:
-                node_short = node.split('|')[len(node.split('|')) - 1]
-            splitted_node = node_short.split(":")
-            splitted_node.pop(len(splitted_node) - 1)
-            self.namespace = ":".join(splitted_node)
-
-        return self.namespace
-
-    def _get_ctrl_name(self, name):
-        """
-        """
-        if self.namespace:
-            return "{}:{}".format(
-                self.namespace,
-                self.ctrl.get(name)
-            )
-        else:
-            return self.ctrl.get(name)
-
-    def _get_camera_shape(self, transform):
-        """
-        """
-        self.camera_shape = None
-        children = cmds.listRelatives(transform, children=True)
-        if children:
-            if cmds.nodeType(children[0]) == "camera":
-                self.camera_shape = children[0]
-
-        return self.camera_shape
-
-    def push_camera_position_to_rig(self):
-        """
-        """
-        # get camera position
-        world_translation = cmds.xform(
-            self.camera, query=True, worldSpace=True, translation=True
-        )
-        world_rotation = cmds.xform(
-            self.camera, query=True, worldSpace=True, rotation=True
-        )
-
-        # set world translation to triangle_ctrl
-        if cmds.objExists(self._get_ctrl_name("triangle")):
-            # set attr
-            cmds.xform(
-                self._get_ctrl_name("triangle"),
-                worldSpace=True,
-                translation=world_translation
-            )
-            cmds.setAttr(
-                "{}.rotate".format(self._get_ctrl_name("triangle")),
-                0,
-                0,
-                0
-            )
-
-        # set world rotation Y to circle
-        if cmds.objExists(self._get_ctrl_name("circle")):
-            # set attr
-            rotation = [0, world_rotation[1], 0]
-            cmds.xform(
-                self._get_ctrl_name("circle"),
-                worldSpace=True,
-                translation=world_translation
-            )
-            cmds.setAttr(
-                "{}.rotate".format(self._get_ctrl_name("circle")),
-                rotation[0],
-                rotation[1],
-                rotation[2]
-            )
-
-        # set world rotation X to square
-        if cmds.objExists(self._get_ctrl_name("square")):
-            # set attr
-            rotation = [world_rotation[0], 0, 0]
-            cmds.xform(
-                self._get_ctrl_name("square"),
-                worldSpace=True,
-                translation=world_translation
-            )
-            cmds.setAttr(
-                "{}.rotate".format(self._get_ctrl_name("square")),
-                rotation[0],
-                rotation[1],
-                rotation[2]
-            )
-
-        # set world rotation Z to cross
-        if cmds.objExists(self._get_ctrl_name("cross")):
-            # set attr
-            rotation = [0, 0, world_rotation[2]]
-            cmds.xform(
-                self._get_ctrl_name("cross"),
-                worldSpace=True,
-                translation=world_translation
-            )
-            cmds.setAttr(
-                "{}.rotate".format(self._get_ctrl_name("cross")),
-                rotation[0],
-                rotation[1],
-                rotation[2]
-            )
-
-        # set target
-        if cmds.objExists(self._get_ctrl_name("target")):
-            # set attr translation same as camera
-            cmds.xform(
-                self._get_ctrl_name("target"),
-                worldSpace=True,
-                translation=world_translation
-            )
-            # set attr translateZ -10
-            cmds.setAttr(
-                "{}.translateZ".format(self._get_ctrl_name("target")),
-                cmds.getAttr("{}.translateZ".format(self._get_ctrl_name("target"))) - 10,
-            )
-
-        # reset value to camera
-        cmds.setAttr("{}.translate".format(self.camera), 0, 0, 0)
-        cmds.setAttr("{}.rotate".format(self.camera), 0, 0, 0)
-
-        return True
-
-
-def push_camera_position_to_rig_selection():
-    """
+def push_camera_position_to_rig():
+    """ push camera position to rig
     """
     selection = cmds.ls(sl=True, l=True)
-    if selection:
-        return push_camera_position_to_rig(camera=selection[0])
+    if not selection:
+        message_box("Please select a camera!")
+        return
 
-    return False
+    # get camera and camera shape
+    camera = selection[0]
+    camera_shape = None
 
-
-def push_camera_position_to_rig(camera):
-    """
-    """
-    if is_a_camera(camera):
-        c = Camera(camera)
-        c.push_camera_position_to_rig()
-        return True
-    else:
-        if not cmds.about(batch=True):
-            kwargs = {
-                "title": "Error",
-                "message": "Please select a camera!",
-                "icon": "information",
-                "button": "OK",
-                "cancelButton": None,
-            }
-            cmds.confirmDialog(**kwargs)
-
-    return False
-
-
-def is_a_camera(node):
-    """
-    check if node provided contain camera directly or as first children.
-    """
-    if cmds.nodeType(node) == "transform":
-        children = cmds.listRelatives(node, children=True)
+    if cmds.nodeType(camera) == "transform":
+        children = cmds.listRelatives(camera, children=True)
         if children:
             if cmds.nodeType(children[0]) == "camera":
-                return True
-    elif cmds.nodeType(node) == "camera":
-        return True
+                camera_shape = children[0]
+    elif cmds.nodeType(camera) == "camera":
+        camera_shape = camera
+        camera = cmds.listRealtives(camera_shape, parent=True)[0]
 
-    return False
+    if not camera_shape:
+        message_box("Please select a camera!")
+        return
+
+    # get namespace if there is one
+    namespace = None
+    if ":" in camera:
+        node_short = camera
+        # split by :
+        if "|" in camera:
+            node_short = camera.split('|')[len(camera.split('|')) - 1]
+        splitted_node = node_short.split(":")
+        splitted_node.pop(len(splitted_node) - 1)
+        namespace = ":".join(splitted_node)
+
+    # get camera position
+    world_translation = cmds.xform(
+        camera, query=True, worldSpace=True, translation=True
+    )
+    world_rotation = cmds.xform(
+        camera, query=True, worldSpace=True, rotation=True
+    )
+
+    # set world translation to triangle_ctrl
+    if cmds.objExists(_get_ctrl_name(namespace, "triangle")):
+        # set attr
+        cmds.xform(
+            _get_ctrl_name(namespace, "triangle"),
+            worldSpace=True,
+            translation=world_translation
+        )
+        cmds.setAttr(
+            "{}.rotate".format(_get_ctrl_name(namespace, "triangle")),
+            0,
+            0,
+            0
+        )
+
+    # set world rotation Y to circle
+    if cmds.objExists(_get_ctrl_name(namespace, "circle")):
+        # set attr
+        rotation = [0, world_rotation[1], 0]
+        cmds.xform(
+            _get_ctrl_name(namespace, "circle"),
+            worldSpace=True,
+            translation=world_translation
+        )
+        cmds.setAttr(
+            "{}.rotate".format(_get_ctrl_name(namespace, "circle")),
+            rotation[0],
+            rotation[1],
+            rotation[2]
+        )
+
+    # set world rotation X to square
+    if cmds.objExists(_get_ctrl_name(namespace, "square")):
+        # set attr
+        rotation = [world_rotation[0], 0, 0]
+        cmds.xform(
+            _get_ctrl_name(namespace, "square"),
+            worldSpace=True,
+            translation=world_translation
+        )
+        cmds.setAttr(
+            "{}.rotate".format(_get_ctrl_name(namespace, "square")),
+            rotation[0],
+            rotation[1],
+            rotation[2]
+        )
+
+    # set world rotation Z to cross
+    if cmds.objExists(_get_ctrl_name(namespace, "cross")):
+        # set attr
+        rotation = [0, 0, world_rotation[2]]
+        cmds.xform(
+            _get_ctrl_name(namespace, "cross"),
+            worldSpace=True,
+            translation=world_translation
+        )
+        cmds.setAttr(
+            "{}.rotate".format(_get_ctrl_name(namespace, "cross")),
+            rotation[0],
+            rotation[1],
+            rotation[2]
+        )
+
+    # set target
+    if cmds.objExists(_get_ctrl_name(namespace, "target")):
+        # set attr translation same as camera
+        cmds.xform(
+            _get_ctrl_name(namespace, "target"),
+            worldSpace=True,
+            translation=world_translation
+        )
+        # set attr translateZ -10
+        cmds.setAttr(
+            "{}.translateZ".format(_get_ctrl_name(namespace, "target")),
+            cmds.getAttr("{}.translateZ".format(
+                _get_ctrl_name(namespace, "target")
+            )) - 10,
+        )
+
+    # reset value to camera
+    cmds.setAttr("{}.translate".format(camera), 0, 0, 0)
+    cmds.setAttr("{}.rotate".format(camera), 0, 0, 0)
+
+    return True
+
+
+def _get_ctrl_name(self, namespace, name):
+    """ get ctrl name
+    """
+    ctrl = {
+        # ctrl
+        "triangle": "triangle_ctrl",
+        "circle": "circle_ctrl",
+        "square": "square_ctrl",
+        "cross": "cross_ctrl",
+        "translateZ": "translateZ_ctrl",
+        "translateY": "translateY_ctrl",
+        # target
+        "target": "target_ctrl",
+        "targetUp": "targetUp_ctrl",
+        # focus
+        "focus": "focus_ctrl"
+    }
+
+    if namespace:
+        return "{}:{}".format(
+            self.namespace,
+            ctrl.get(name)
+        )
+    else:
+        return ctrl.get(name)
+
+
+def message_box(message=""):
+    """ a simple message box
+    """
+    if cmds.about(batch=True):
+        return
+
+    kwargs = {
+        "title": "Error",
+        "message": message,
+        "icon": "information",
+        "button": "OK",
+        "cancelButton": None,
+    }
+    cmds.confirmDialog(**kwargs)
