7954649170560144b9c84911feac7b8722018228
Logan Delmay <ldelmay@ws142.prs.vfx.int>
2022-05-03 17:25:09 +0200

    alt using scene bounding box's diagonal length

--- openpype/modules/quad/set_near_far.py
+++ openpype/modules/quad/set_near_far.py
@@ -1,4 +1,5 @@
 import maya.cmds as cmds
+import math
 
 def set_near_far_based_on_scene():
 	"""
@@ -43,6 +44,32 @@ def set_near_far_based_on_scene_no_int_cast():
 
 	min_value = maxSize/3333.3333
 	max_value = maxSize*3
+	if min_value < 0.001:
+		min_value = 0.001
+	
+	for c in cameras:
+		cmds.setAttr("{c}.nearClipPlane".format(c=c), min_value)
+		cmds.setAttr("{c}.farClipPlane".format(c=c), max_value)
+
+	return True
+
+def set_near_far_based_on_scene_diagonal():
+	"""
+	"""
+	sl = cmds.ls(assemblies=True)
+
+	bbox = cmds.exactWorldBoundingBox(sl, ignoreInvisible=True)
+	size = []
+	size.append(bbox[3]-bbox[0])
+	size.append(bbox[4]-bbox[1])
+	size.append(bbox[5]-bbox[2])
+	maxSize = math.sqrt(size[0]*size[0]+size[1]*size[1]+size[2]*size[2])*2
+
+	# get cameras
+	cameras = cmds.ls(type="camera")
+
+	max_value = maxSize
+	min_value = maxSize/10000.0
 	if min_value < 0.001:
 		min_value = 0.001
 	
