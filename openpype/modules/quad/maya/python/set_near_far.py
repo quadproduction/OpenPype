2d80c314123f2b09d2c71d5a5425a0f62bc3ee90
Logan Delmay <ldelmay@ws142.prs.vfx.int>
2022-05-10 12:33:38 +0200

    updated to go over all frames and keep the maximum bbox diagonal length

--- openpype/modules/quad/maya/python/set_near_far.py
+++ openpype/modules/quad/maya/python/set_near_far.py
@@ -1,77 +1,32 @@
 import maya.cmds as cmds
 import math
 
-def set_near_far_based_on_scene():
+
+def set_near_far():
 	"""
 	"""
+	animStart = cmds.playbackOptions(query=True,animationStartTime=True)
+	animEnd = cmds.playbackOptions(query=True,animationEndTime=True) 
+
 	sl = cmds.ls(assemblies=True)
+	maxSize = 0.0
 
-	bbox = cmds.exactWorldBoundingBox(sl, ignoreInvisible=True)
-	size = []
-	size.append(bbox[3]-bbox[0])
-	size.append(bbox[4]-bbox[1])
-	size.append(bbox[5]-bbox[2])
-	maxSize = max(size)
+	for frame in range(int(animStart),int(animEnd)):
+		cmds.currentTime(frame)
+		bbox = cmds.exactWorldBoundingBox(sl, ignoreInvisible=True)
+		size = []
+		size.append(bbox[3]-bbox[0])
+		size.append(bbox[4]-bbox[1])
+		size.append(bbox[5]-bbox[2])
+		maxSize = max(maxSize,math.sqrt(size[0]*size[0]+size[1]*size[1]+size[2]*size[2])*2)
 
-	# get cameras
-	cameras = cmds.ls(type="camera")
-
-	min_value = int(maxSize*3/10000.0)
-	max_value = int(maxSize*3)
-	if min_value < 0.001:
-		min_value = 0.001
-	
-	for c in cameras:
-		cmds.setAttr("{c}.nearClipPlane".format(c=c), min_value)
-		cmds.setAttr("{c}.farClipPlane".format(c=c), max_value)
-
-	return True
-
-def set_near_far_based_on_scene_no_int_cast():
-	"""
-	"""
-	sl = cmds.ls(assemblies=True)
-
-	bbox = cmds.exactWorldBoundingBox(sl, ignoreInvisible=True)
-	size = []
-	size.append(bbox[3]-bbox[0])
-	size.append(bbox[4]-bbox[1])
-	size.append(bbox[5]-bbox[2])
-	maxSize = max(size)
-
-	# get cameras
-	cameras = cmds.ls(type="camera")
-
-	min_value = maxSize/3333.3333
-	max_value = maxSize*3
-	if min_value < 0.001:
-		min_value = 0.001
-	
-	for c in cameras:
-		cmds.setAttr("{c}.nearClipPlane".format(c=c), min_value)
-		cmds.setAttr("{c}.farClipPlane".format(c=c), max_value)
-
-	return True
-
-def set_near_far_based_on_scene_diagonal():
-	"""
-	"""
-	sl = cmds.ls(assemblies=True)
-
-	bbox = cmds.exactWorldBoundingBox(sl, ignoreInvisible=True)
-	size = []
-	size.append(bbox[3]-bbox[0])
-	size.append(bbox[4]-bbox[1])
-	size.append(bbox[5]-bbox[2])
-	maxSize = math.sqrt(size[0]*size[0]+size[1]*size[1]+size[2]*size[2])*2
-
+		
 	# get cameras
 	cameras = cmds.ls(type="camera")
 
 	max_value = maxSize
 	min_value = maxSize/3333.3333
-	if min_value < 0.001:
-		min_value = 0.001
+	min_value = max(min_value,0.001)
 	
 	for c in cameras:
 		cmds.setAttr("{c}.nearClipPlane".format(c=c), min_value)
