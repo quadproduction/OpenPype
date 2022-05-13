2e345a2272f961ed3352e115ca4b43bff352560b
clement.hector <clement.hector@fixstudio.com>
2022-05-13 18:05:49 +0200

    move file

--- openpype/modules/quad/host/maya/python/set_near_far.py
+++ openpype/modules/quad/host/maya/python/set_near_far.py
@@ -20,14 +20,14 @@ def set_near_far():
 		size.append(bbox[5]-bbox[2])
 		maxSize = max(maxSize,math.sqrt(size[0]*size[0]+size[1]*size[1]+size[2]*size[2])*2)
 
-		
+
 	# get cameras
 	cameras = cmds.ls(type="camera")
 
 	max_value = maxSize
 	min_value = maxSize/3333.3333
 	min_value = max(min_value,0.001)
-	
+
 	for c in cameras:
 		cmds.setAttr("{c}.nearClipPlane".format(c=c), min_value)
 		cmds.setAttr("{c}.farClipPlane".format(c=c), max_value)
