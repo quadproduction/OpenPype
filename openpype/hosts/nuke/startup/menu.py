93d72495c39c1b30f931e571686466e73dc5cff8
clement.hector <clement.hector@fixstudio.com>
2022-05-17 15:02:11 +0200

    Squashed commit of the following:
    commit ac82e0107686527157d56b2f748abf4a4b92f6c3
    Author: clement.hector <clement.hector@fixstudio.com>
    Date:   Thu May 12 15:55:11 2022 +0200
    
        fix string format for python2
    
    commit 542379ec1dac11ddb0704eb4e1b2516450591c9a
    Author: Thomas Fricard <tfricard@ws114.prs.vfx.int>
    Date:   Thu May 12 11:20:55 2022 +0200
    
        add the docs for the gizmo menu
    
    commit bf12d5e5e928d0d02002401aff73c9786958034d
    Author: Thomas Fricard <tfricard@ws114.prs.vfx.int>
    Date:   Thu May 12 11:12:08 2022 +0200
    
        set the default gizmo to a sticky note
    
    commit 0194ba61b72841756757feb5e1a3528e8425395f
    Author: Thomas Fricard <tfricard@ws114.prs.vfx.int>
    Date:   Wed May 11 11:46:57 2022 +0200
    
        fix install_host import
    
    commit d3b415431afbde4c561a19693b93db6966d7a84d
    Author: Thomas Fricard <tfricard@ws114.prs.vfx.int>
    Date:   Mon May 9 15:13:50 2022 +0200
    
        add gizmo_menu module
    
    commit b557302820381cb0e9d260ec63436dcbbeb09cfc
    Author: Thomas Fricard <tfricard@ws114.prs.vfx.int>
    Date:   Mon May 9 15:12:16 2022 +0200
    
        refactor default gizmo
    
    commit f7ad121c5e28ce8a20db9a2b84ea230b04a891d3
    Author: Thomas Fricard <tfricard@ws114.prs.vfx.int>
    Date:   Wed May 4 17:01:32 2022 +0200
    
        generate toolbar menu from openpype project settings
    
    commit d9d08b3e96f02b2f6928dd0e3ada3139c94a5935
    Author: Thomas Fricard <tfricard@ws114.prs.vfx.int>
    Date:   Wed May 4 12:16:19 2022 +0200
    
        add a tab in nuke project settings for gizmos

--- openpype/hosts/nuke/startup/menu.py
+++ openpype/hosts/nuke/startup/menu.py
@@ -57,5 +57,60 @@ def add_scripts_menu():
     # apply configuration
     studio_menu.build_from_configuration(studio_menu, config)
 
+def add_scripts_gizmo():
+    try:
+        from openpype.hosts.nuke.api import lib
+    except ImportError:
+        log.warning(
+            "Skipping studio.gizmo install, because "
+            "'scriptsgizmo' module seems unavailable."
+        )
+        return
 
+    # load configuration of custom menu
+    project_settings = get_project_settings(os.getenv("AVALON_PROJECT"))
+
+    for gizmo in project_settings["nuke"]["gizmo"]:
+        config = gizmo["gizmo_definition"]
+        toolbar_name = gizmo["toolbar_menu_name"]
+        gizmo_path = gizmo["gizmo_path"]
+        icon = gizmo['toolbar_icon_path']
+
+        if not any(gizmo_path):
+            log.warning("Skipping studio gizmo, no gizmo path found.")
+            return
+
+        if not config:
+            log.warning("Skipping studio gizmo, no definition found.")
+            return
+
+        try:
+            icon = icon.format(**os.environ)
+        except KeyError as e:
+            log.warning(f"This environment variable doesn't exist: {e}")
+
+        for gizmo in gizmo_path:
+            try:
+                gizmo = gizmo.format(**os.environ)
+                gizmo_path.append(gizmo)
+                gizmo_path.pop(0)
+            except KeyError as e:
+                log.warning(f"This environment variable doesn't exist: {e}")
+
+        nuke_toolbar = nuke.menu("Nodes")
+        toolbar = nuke_toolbar.addMenu(toolbar_name, icon=icon)
+
+        # run the launcher for Nuke toolbar
+        studio_menu = lib.gizmo_creation(
+            title=toolbar_name,
+            parent=toolbar,
+            objectName=toolbar_name.lower().replace(" ", "_"),
+            icon=icon
+        )
+
+        # apply configuration
+        studio_menu.add_gizmo_path(gizmo_path)
+        studio_menu.build_from_configuration(toolbar, config)
+
+add_scripts_gizmo()
 add_scripts_menu()
--- website/docs/admin_hosts_nuke.md
+++ website/docs/admin_hosts_nuke.md
@@ -8,6 +8,10 @@ sidebar_label: Nuke
 You can add your custom tools menu into Nuke by extending definitions in **Nuke -> Scripts Menu Definition**.
 ![Custom menu definition](assets/nuke-admin_scriptsmenu.png)
 
+## Custom Menu
+You can add your custom toolbar menu into Nuke by setting your gizmo path and extending definitions in **Nuke -> Gizmo Menu**.
+![Custom menu definition](assets/nuke-admin_gizmomenu.png)
+
 :::note Work in progress
 This is still work in progress. Menu definition will be handled more friendly with widgets and not
 raw json.
