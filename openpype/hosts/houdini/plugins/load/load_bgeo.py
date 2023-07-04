4d9a13c4a2a434c0eb7e45bf3689598c7ea6ac09
EvanLambert <evan.lambert.3d@gmail.com>
2023-07-04 15:59:19 +0200

    retake first test

--- openpype/hosts/houdini/plugins/load/load_bgeo.py
+++ openpype/hosts/houdini/plugins/load/load_bgeo.py
@@ -13,7 +13,7 @@ class BgeoLoader(load.LoaderPlugin):
     """Load bgeo files to Houdini."""
 
     label = "Load bgeo"
-    families = ["model", "pointcache", "bgeocache"]
+    families = ["model", "pointcache", "bgeo", "bgeocache"]
     representations = [
         "bgeo", "bgeosc", "bgeogz",
         "bgeo.sc", "bgeo.gz", "bgeo.lzma", "bgeo.bz2"]
