8285dc0ab638a56f96fb3e50afa96ec6be52c3f4
Thomas Fricard <tfricard@ws114.prs.vfx.int>
2023-10-18 15:45:24 +0200

    set default value for symlink and pass the ui values to the data

--- openpype/hosts/hiero/api/plugin.py
+++ openpype/hosts/hiero/api/plugin.py
@@ -668,6 +668,7 @@ class PublishClip:
     count_steps_default = 10
     vertical_sync_default = False
     driving_layer_default = ""
+    symlink_default = False
 
     def __init__(self, cls, track_item, **kwargs):
         # populate input cls attribute onto self.[attr]
@@ -697,7 +698,8 @@ class PublishClip:
         # adding ui inputs if any
         self.ui_inputs = kwargs.get("ui_inputs", {})
 
-        self.symlink = self.ui_inputs['hierarchyData']['value'].get('symlink')
+        project_settings = get_current_project_settings()
+        self.symlink = project_settings["hiero"]["create"]["CreateShotClip"]["symlink"]  # noqa
 
         # populate default data before we get other attributes
         self._populate_track_item_default_data()
@@ -754,7 +756,7 @@ class PublishClip:
             "_clip_": self.ti_name,
             "_trackIndex_": self.track_index,
             "_clipIndex_": self.ti_index,
-            "_symlink_": self.symlink["value"]
+            "_symlink_": self.symlink
         }
 
     def _populate_attributes(self):
@@ -778,7 +780,11 @@ class PublishClip:
         self.hierarchy_data = self.ui_inputs.get(
             "hierarchyData", {}).get("value") or \
             self.track_item_default_data.copy()
-        self.hierarchy_data["symlink"].update({"value": "{_symlink_}"})
+
+        ui_symlink = self.ui_inputs.get(
+            "hierarchyData", {}).get("value").get("symlink").get("value")
+        self.hierarchy_data["symlink"].update({"value": str(ui_symlink)})
+
         self.count_from = self.ui_inputs.get(
             "countFrom", {}).get("value") or self.count_from_default
         self.count_steps = self.ui_inputs.get(
