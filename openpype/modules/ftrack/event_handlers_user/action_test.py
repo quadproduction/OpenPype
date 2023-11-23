82464cc0fb346fe92d27ccb79011dbbf2ab6ee8b
Thomas Fricard <tfricard@ws114.prs.vfx.int>
2023-11-23 14:37:47 +0100

    Action class currently not used. Sync status of projects from ftrack to avalon. (quadproduction/issues#162)

--- openpype/modules/ftrack/event_handlers_user/action_test.py
+++ openpype/modules/ftrack/event_handlers_user/action_test.py
@@ -1,26 +1,55 @@
+from pymongo import UpdateOne
+
 from openpype_modules.ftrack.lib import BaseAction, statics_icon
+from openpype.client import get_projects, get_project
+from openpype.pipeline import AvalonMongoDB
 
 
 class TestAction(BaseAction):
-    """Action for testing purpose or as base for new actions."""
-
-    ignore_me = True
-
+    """Action for testing purpose or as base for new actions.
+    Action currently not used but meant to sync status of projects"""
     identifier = 'test.action'
     label = 'Test action'
     description = 'Test action'
-    priority = 10000
-    role_list = ['Pypeclub']
     icon = statics_icon("ftrack", "action_icons", "TestAction.svg")
+
+    def __init__(self, session):
+        self.dbcon = AvalonMongoDB()
+        super().__init__(session)
 
     def discover(self, session, entities, event):
         return True
 
     def launch(self, session, entities, event):
-        self.log.info(event)
+        projects_to_be_deactived = []
+        disabled_ftrack_projects = session.query(
+            "select id from Project where status is_not active"
+        ).all()
+
+        disabled_ftrack_projects_id = [
+            project['id'] for project in disabled_ftrack_projects
+        ]
+        mongo_projects = get_projects()
+
+        for project in mongo_projects:
+            if project['data'].get('ftrackId') in disabled_ftrack_projects_id:
+                projects_to_be_deactived.append(project)
+                self.log.debug(project['name'])
+
+        # mongo_changes_bulk = []
+        for project in projects_to_be_deactived:
+            filter = {"_id": project["_id"]}
+            change_data = {"$set": {'data.active': False}}
+            self.dbcon.Session["AVALON_PROJECT"] = project['name']
+            self.dbcon.bulk_write([UpdateOne(filter, change_data)])
+            # mongo_changes_bulk.append(UpdateOne(filter, change_data))
+
+        # if mongo_changes_bulk:
+        #     self.dbcon.bulk_write(mongo_changes_bulk)
 
         return True
 
 
 def register(session):
-    TestAction(session).register()
+    # TestAction(session).register()
+    pass
