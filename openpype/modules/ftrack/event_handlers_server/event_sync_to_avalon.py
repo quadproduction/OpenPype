a2f12ea36091d14d31e988c100c93fdad1729eeb
Thomas Fricard <tfricard@ws114.prs.vfx.int>
2023-11-23 14:47:07 +0100

    Convert name and value of key "status" to match "active" key in avalon. (quadproduction/issues#162)

--- openpype/modules/ftrack/event_handlers_server/event_sync_to_avalon.py
+++ openpype/modules/ftrack/event_handlers_server/event_sync_to_avalon.py
@@ -1953,13 +1953,23 @@ class SyncToAvalonEvent(BaseEvent):
                     self.hier_cust_attrs_changes[key].append(ftrack_id)
                     continue
 
-                if key not in ent_cust_attrs:
+                # This is because "status" is not in custom attributes
+                # but it's a "properties" present in every project
+                # so this will not skip the key
+                if key != "status" and key not in ent_cust_attrs:
                     continue
 
                 value = values["new"]
-                new_value = self.convert_value_by_cust_attr_conf(
-                    value, ent_cust_attrs[key]
-                )
+                # Rename the key because the "status" in ftrack
+                # is called "active" in avalon and is a boolean
+                if key == "status":
+                    key = "active"
+                    new_value = True if value == "active" else False
+                    ent_path = "Project"
+                else:
+                    new_value = self.convert_value_by_cust_attr_conf(
+                        value, ent_cust_attrs[key]
+                    )
 
                 if entType == "show" and key == "applications":
                     # Store apps to project't config
@@ -2672,7 +2682,7 @@ class SyncToAvalonEvent(BaseEvent):
         for ftrack_id, mongo_id in ftrack_mongo_mapping_found.items():
             filter = {"_id": mongo_id}
             change_data = {"$set": {}}
-            change_data["$set"]["data.tasks"] = tasks_per_ftrack_id[ftrack_id]
+            change_data["$set"]["data.active"] = False
             mongo_changes_bulk.append(UpdateOne(filter, change_data))
 
         if mongo_changes_bulk:
