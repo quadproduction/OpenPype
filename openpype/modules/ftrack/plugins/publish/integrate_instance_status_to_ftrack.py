e08570deaadb82843db1b140aa9a031f5db46d1b
Thomas Fricard <tfricard@ws114.prs.vfx.int>
2024-01-19 16:04:19 +0100

    fix how to get the status (quadproduction/issues#203)

--- openpype/modules/ftrack/plugins/publish/integrate_instance_status_to_ftrack.py
+++ openpype/modules/ftrack/plugins/publish/integrate_instance_status_to_ftrack.py
@@ -6,6 +6,7 @@ import pyblish.api
 
 from openpype.settings import get_current_project_settings
 from openpype.modules.ftrack import get_asset_version_by_task_id
+from openpype.modules.ftrack.lib import get_ftrack_statuses
 
 
 class IntegrateInstanceStatusToFtrack(pyblish.api.InstancePlugin):
@@ -16,8 +17,6 @@ class IntegrateInstanceStatusToFtrack(pyblish.api.InstancePlugin):
     active = True
 
     def process(self, instance):
-        self.log.debug(f"SATUTS_TO_SET: {instance.data['creator_attributes'].get('ftrack_status')}")
-
         task = instance.data.get("ftrackTask")
         name = instance.data.get("name")
         session = instance.context.data["ftrackSession"]
@@ -43,14 +42,26 @@ class IntegrateInstanceStatusToFtrack(pyblish.api.InstancePlugin):
             return
 
         instance_status = instance.data['creator_attributes'].get(
-            'ftrack_status'
+            'ftrackStatus'
         )
-        asset_version_data["status"] = instance_status
+        if not instance_status:
+            instance_status = "N/A"
+        project_name = instance.context.data.get("projectName")
+        statuses = get_ftrack_statuses(project_name)
+        status = [status for status in statuses if status['name'] == instance_status]  # noqa
+
+        if not status:
+            self.log.debug(
+                "Status \"{}\" not found in Ftrack".format(instance_status)
+            )
+            return
+
+        asset_version_data["status"] = status[0]
 
         try:
-            # session.commit()
+            session.commit()
             self.log.debug(
-                "Status set to {} for AssetVersion \"{}\" ".format(
+                "Status set to \"{}\" for AssetVersion \"{}\" ".format(
                     instance_status, str(asset_version_data)
                 )
             )
