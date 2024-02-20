0593857b2feb4a7c4d8b6241b88fc6636f58396f
Thomas Fricard <tfricard@ws114.prs.vfx.int>
2024-02-20 17:55:02 +0100

    create the extract mantra ifd (quadproduction/issues#253)

---
+++ openpype/hosts/houdini/plugins/publish/extract_mantra_ifd.py
@@ -0,0 +1,50 @@
+import os
+import hou
+import pyblish.api
+
+from openpype.pipeline import publish
+
+
+class ExtractMantraIFD(publish.Extractor):
+    order = pyblish.api.ExtractorOrder
+    label = "Extract Mantra IFD"
+    hosts = ["houdini"]
+    families = ["mantraifd"]
+    targets = ["local", "remote"]
+
+    def process(self, instance):
+        if instance.data.get("farm"):
+            self.log.debug("Should be processed on farm, skipping.")
+            return
+
+        ropnode = hou.node(instance.data.get("instance_node"))
+        output = ropnode.evalParm("soho_diskfile")
+        staging_dir = os.path.dirname(output)
+        instance.data["stagingDir"] = staging_dir
+
+        files = instance.data["frames"]
+        missing_frames = [
+            frame
+            for frame in files
+            if not os.path.exists(
+                os.path.normpath(os.path.join(staging_dir, frame))
+            )
+        ]
+        if missing_frames:
+            raise RuntimeError(
+                "Failed to complete Mantra IFD extraction. "
+                "Missing output files: {}".format(missing_frames)
+            )
+
+        if "representations" not in instance.data:
+            instance.data["representations"] = []
+
+        representation = {
+            "name": "ifd",
+            "ext": "ifd",
+            "files": files,
+            "stagingDir": staging_dir,
+            "frameStart": instance.data["frameStart"],
+            "frameEnd": instance.data["frameEnd"]
+        }
+        instance.data["representations"].append(representation)
