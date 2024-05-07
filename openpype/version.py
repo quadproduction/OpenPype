9edb01a6103156c2f6a300f42015045e75c8dd73
Guilhemz <146825030+Guilhemz@users.noreply.github.com>
2024-05-07 15:42:09 +0200

    Update version.py

--- openpype/version.py
+++ openpype/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 """Package declaring Pype version."""
 
-__version__ = "staging/3.16.9-quad-1.11.2-bypass-resolution-reset"
+__version__ = "3.16.9-quad-1.11.2-bypass-resolution-reset"
