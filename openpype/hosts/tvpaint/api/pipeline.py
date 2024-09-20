289199430327ccda36f061cb98df1abf751a764a
Cyprien CAILLOT <cyprien.caillot@quad.fr>
2024-09-20 09:43:20 +0200

    improve path windows managing

--- openpype/hosts/tvpaint/api/pipeline.py
+++ openpype/hosts/tvpaint/api/pipeline.py
@@ -2,11 +2,12 @@ import os
 import json
 import tempfile
 import logging
-
+import sys
 import requests
-
 import pyblish.api
-
+import ctypes
+from ctypes import wintypes
+from pathlib import Path
 from openpype.client import get_asset_by_name
 from openpype.host import HostBase, IWorkfileHost, ILoadHost, IPublishHost
 from openpype.hosts.tvpaint import TVPAINT_ROOT_DIR
@@ -145,8 +146,9 @@ class TVPaintHost(HostBase, IWorkfileHost, ILoadHost, IPublishHost):
 
     # --- Workfile ---
     def open_workfile(self, filepath):
+        filepath = self.optimize_path_compatibility(filepath).replace("\\", "/")
         george_script = "tv_LoadProject '\"'\"{}\"'\"'".format(
-            filepath.replace("\\", "/")
+            filepath
         )
         return execute_george_through_file(george_script)
 
@@ -155,10 +157,40 @@ class TVPaintHost(HostBase, IWorkfileHost, ILoadHost, IPublishHost):
             filepath = self.get_current_workfile()
         context = get_global_context()
         save_current_workfile_context(context)
-
         # Execute george script to save workfile.
-        george_script = "tv_SaveProject {}".format(filepath.replace("\\", "/"))
+        filepath = self.optimize_path_compatibility(filepath).replace("\\", "/")
+        george_script = u"tv_SaveProject {}".format(filepath)
         return execute_george(george_script)
+
+    @staticmethod
+    def optimize_path_compatibility(filepath):
+        workfile_path = Path(filepath)
+        workfile_path.parent.mkdir(parents=True, exist_ok=True)
+
+        if 'win' not in sys.platform:
+            log.info("improve_compatibility: nothing done, only applicable for Windows.")
+            return filepath
+
+        # Windows-specific logic to convert the filepath to its short path form.
+        _GetShortPathNameW = ctypes.windll.kernel32.GetShortPathNameW
+        _GetShortPathNameW.argtypes = [wintypes.LPCWSTR, wintypes.LPWSTR, wintypes.DWORD]
+        _GetShortPathNameW.restype = wintypes.DWORD
+
+        workfile_parent = str(workfile_path.parent)
+        output_buf_size = len(workfile_parent)
+        # Iteratively try to get the short path name, increasing buffer size if needed.
+        while True:
+            output_buf = ctypes.create_unicode_buffer(output_buf_size)
+            needed_size = _GetShortPathNameW(workfile_parent, output_buf, output_buf_size)
+            if needed_size == 0:
+                raise ctypes.WinError()
+            if output_buf_size >= needed_size:
+                return os.path.join(output_buf.value, workfile_path.name)  # Return the short path version.
+            else:
+                output_buf_size = needed_size  # Adjust the buffer size if needed.
+
+        # Fallback return to the original path if for some reason Windows-specific logic fails.
+        return str(workfile_path)
 
     def work_root(self, session):
         return session["AVALON_WORKDIR"]
