f930d43340f0389001ecbc5f402997fb8527dbe1
Ben Souchet <contact@bensouchet.dev>
2024-11-06 15:28:27 +0100

    Fix issues with  command executions fr tvpaint

--- openpype/hosts/tvpaint/api/pipeline.py
+++ openpype/hosts/tvpaint/api/pipeline.py
@@ -155,8 +155,8 @@ class TVPaintHost(HostBase, IWorkfileHost, ILoadHost, IPublishHost):
         save_current_workfile_context(context)
         # Execute george script to save workfile.
         filepath = filepath.replace("\\", "/")
-        george_script = u"tv_SaveProject '\"{}\"'".format(filepath)
-        return execute_george(george_script)
+        george_script = u"tv_SaveProject '{}'".format(filepath)
+        return execute_george_through_file(george_script)
 
     def work_root(self, session):
         return session["AVALON_WORKDIR"]
